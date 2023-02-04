098bb9748becf3ec3797ea981fa97b96ae54150a
@anhnv99 <vietanhhappy99@gmail.com>
2023-02-04 14:43:27 +0700

    first commit

---
+++ src/main/java/com/example/project_sem_4/database/dto/booking/BookingSearchDTO.java
@@ -0,0 +1,36 @@
+package com.example.project_sem_4.database.dto.booking;
+
+import com.example.project_sem_4.database.entities.Booking;
+import com.example.project_sem_4.database.entities.Role;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+public class BookingSearchDTO {
+    public BookingSearchDTO(){
+    }
+    @Getter
+    @Setter
+    public static class Employee{
+
+        private String employee_name;
+        private String employee_thumbnail;
+        private String employee_description;
+        private List<Role> roles;
+
+        private List<Booking> BookingByTime_bookings;
+
+        public Employee() {
+        }
+    }
+    public Employee employee;
+    public BookingSearchDTO(Employee emp){
+        this.employee = emp;
+    }
+}
